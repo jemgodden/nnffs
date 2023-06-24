d1a2ffb8f02d16948fffce41d6cda256a77f7595
jemgodden <jemgodden@gmail.com>
2023-06-24 15:13:06 +0100

    Include corrections

--- src/network/activations.cpp
+++ src/network/activations.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 
-#include "../../include/nn/activations.h"
-#include "../../include/nn/nn.h"
+#include "../../include/network/activations.h"
+#include "../../include/network/network.h"
 
 
 double sigmoid(double input) {
